9cf981b0e0f1ea2fd36216c32907e12b2bdee14a
morantaf <fall_moranta@live.fr>
2020-07-21 15:39:59 +0200

    Creating the function to fetch articles and to dispatch them to the store

--- src/store/actions/news.js
+++ src/store/actions/news.js
@@ -2,10 +2,22 @@ import request from "superagent";
 
 const API_KEY = "8b5b038fd1344e60b706a1fe04d8ddc8";
 
-const baseUrl = `newsapi.org/v2/everything?q=football&from=2020-07-09&sortBy=publishedAt&apiKey=${API_KEY}`;
+const baseUrl = `http://newsapi.org/v2/everything?q=football&from=2020-07-09&sortBy=publishedAt&apiKey=${API_KEY}`;
 
-export const getImages = () => async (dispatch) => {
+const allArticles = (payload) => ({
+  type: "ALL_ARTICLES",
+  payload: payload,
+});
+
+export const getArticles = () => async (dispatch) => {
   try {
+    const response = await request.get(`${baseUrl}`);
+
+    const articles = response.body;
+
+    const action = allArticles(articles);
+
+    dispatch(action);
   } catch (error) {
     console.error(error);
   }
